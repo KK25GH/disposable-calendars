8bd6c2c8261cad5aee2dd0b54aa60a189ece4d7d
KAKU25GH <kakumorikoki25@gmail.com>
2023-08-20 17:52:19 +0900

    テーブルを２つ作成
    calendars, memos

--- app/database/migrations/2023_08_20_034719_create_calendars_table.php
+++ app/database/migrations/2023_08_20_034719_create_calendars_table.php
@@ -3,25 +3,40 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
-return new class extends Migration
+class CreateCalendarsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
+        // カレンダーのデータベースを作成
         Schema::create('calendars', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // idカラム
+            $table->foreignId('user_id') // 外部キーを追加
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('name'); // nameカラム
+            $table->integer('year'); // yearカラム
+            $table->integer('month'); // monthカラム
+            $table->timestamps(); // created_atとupdated_atカラム
         });
+
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
+        // データベースを削除
         Schema::dropIfExists('calendars');
     }
-};
+}
--- app/database/migrations/2023_08_20_034822_create_memos_table.php
+++ app/database/migrations/2023_08_20_034822_create_memos_table.php
@@ -3,25 +3,38 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Calendar;
 
-return new class extends Migration
+class CreateMemosTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
+        // メモのデータベースを作成
         Schema::create('memos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // idカラム
+            $table->foreignId('calendar_id') // calendar_idカラム（外部キー）
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->date('date'); // dateカラム
+            $table->text('memo'); // memoカラム
+            $table->timestamps(); // created_atとupdated_atカラム
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
+        // データベースを削除
         Schema::dropIfExists('memos');
     }
-};
+}
