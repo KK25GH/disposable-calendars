49fe04dca9500bae25783caf72dbce31530590fa
KAKU25GH <kakumorikoki25@gmail.com>
2023-08-22 23:45:10 +0900

    CalendarControllerを編集
    ログイン認証の確認
    最初に表示するデータを取得

--- app/app/Http/Controllers/CalendarController.php
+++ app/app/Http/Controllers/CalendarController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class CalendarController extends Controller
 {
-    //
+    /**
+     * ログインしているかを確認する。
+     * している場合は、TodoControllerへアクセスできる。
+     * していない場合は、ログインすることを要求する。
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * ログインしているユーザーのカレンダーとメモを表示する
+     */
+    public function index(Request $request)
+    {
+        //カレンダーを作ったのが最も最近なのを表示、hasがascだったら古いやつを表示
+        if($request->has('asc')) {
+            $calendars = Calendar::where(['user_id' => Auth::id() ])
+                ->orderBy('created_at','asc')
+                ->get();
+        } else {
+            //最初のリダイレクトでは、何も代入されていないので、elseの処理
+            $calendars = Calendar::where(['user_id' => Auth::id() ])
+                ->orderBy('created_at','desc')
+                ->get();
+        }
+    }
+
 }
+
+
