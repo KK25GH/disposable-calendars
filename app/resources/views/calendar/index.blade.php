64ff4808573a4d0ba53a854c997ad24a081dd85b
KAKU25GH <kakumorikoki25@gmail.com>
2023-08-29 22:33:03 +0900

    カレンダーレイアウトの調整
    カレンダーを画面に大きく表示する

--- app/resources/views/calendar/index.blade.php
+++ app/resources/views/calendar/index.blade.php
@@ -13,8 +13,11 @@
 
 @extends('layouts.app')
 @section('content')
-<div class="container">
+<div class="container-fluid">
    <div class="row justify-content-center">
+        <div class ="col-md-8">
+            <p class="create">編集　　新規作成</p>
+        </div>
        <div class="col-md-8">
            <div class="card">
                <div class="card-header">{{ $calendar->getTitle() }}</div>
