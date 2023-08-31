bdeb20563287ba0f1694fed6cf1f823605355078
KAKU25GH <kakumorikoki25@gmail.com>
2023-08-31 21:48:47 +0900

    カレンダーリストとアイテム例を作成
    グリッドシステムを行と列の修正
    カレンダーリストとアイテムのモックアップを作成

--- app/resources/views/calendar/index.blade.php
+++ app/resources/views/calendar/index.blade.php
@@ -1,23 +1,11 @@
-{{-- @extends('layouts.app')
-
-
- 下記に表示したカレンダー以外の選択肢をforeachで並べていく。
-@section('content')
-<div>aa</div>
-
- イメージとして、カレンダー１つ
-
-@endsection
-
---}}
-
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid">
    <div class="row justify-content-center">
-        <div class ="col-md-8">
-            <p class="create">編集　　新規作成</p>
-        </div>
+        <div class="col-md-8"><p class="create">編集　新規作成</p></div>
+   </div>
+   <div class="row justify-content-center">
+       <div class="col-md-2"></div>
        <div class="col-md-8">
            <div class="card">
                <div class="card-header">{{ $calendar->getTitle() }}</div>
@@ -25,6 +13,23 @@
 					{!! $calendar->render() !!}
                </div>
            </div>
+       </div>
+       <div class="col-md-2">
+            <div class="card">
+                <div class="card-header">カレンダーリスト</div>
+                    {{-- item example --}}
+                    <div class="list-group-scroll">
+                        <div class="list-group list-group-flush">
+                          <a href="#" class="list-group-item list-group-item-action">An item</a>
+                          <a href="#" class="list-group-item list-group-item-action">A second item</a>
+                          <a href="#" class="list-group-item list-group-item-action">A third item</a>
+                          <a href="#" class="list-group-item list-group-item-action">A fourth item</a>
+                          <!-- more items -->
+                        </div>
+                      </div>
+
+                </div>
+            </div>
        </div>
    </div>
 </div>
