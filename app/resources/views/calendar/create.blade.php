657fd6b0faddd6f57637e901e34786107a7f19cf
KAKU25GH <kakumorikoki25@gmail.com>
2023-09-08 23:50:54 +0900

    カレンダー新規作成画面の準備
    HTMLとSCSSの調整

--- app/resources/views/calendar/create.blade.php
+++ app/resources/views/calendar/create.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.app')
 @section('content')
-
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header"><h1 class="ymh1">２０２３年　　　　５月</h1></div>
+                    </div>
+            </div>
+                <div class="col-md-8">
+                    <div class="CandS">
+                        <a>キャンセル</a><a>保存</a>
+                    </div>
+                </div>
+        </div>
+    </div>
 @endsection
