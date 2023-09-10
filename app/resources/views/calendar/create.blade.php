de507234e0942b82d6df82cca99c6dbf61464cc4
KAKU25GH <kakumorikoki25@gmail.com>
2023-09-10 21:33:23 +0900

    パーツの追加とCSSの調整
    タイトル入力追加
    キャンセル、作成　ボタン追加
    余白を調整

--- app/resources/views/calendar/create.blade.php
+++ app/resources/views/calendar/create.blade.php
@@ -3,36 +3,53 @@
     <div class="create-vertical-center">
         <div class="container-fluid">
             <div class="row justify-content-center">
-                <div class="col-md-8">
+                <div class="col-md-8 margin-bottom">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="center">タイトルを入力してください。</h5>
+                        </div>
+                        <div class="center">
+                            <input class="c_title" type="text">
+                        </div>
+                    </div>
+            </div>
+                <div class="col-md-8 margin-bottom">
                         <div class="card">
-                            <div class="card-header">
-                                <h1 class="ymh1">
-
-                                    <select name="" id="">
-                                        @for($i=0; $i<50; $i++) {
-                                            <option value="{{$thisYear + $i}}">{{$thisYear + $i}}</option>
-                                        }
-                                        @endfor
-                                    </select>
-
-                                    年
-
-                                    <select name="" id="">
-                                        @for($i=1; $i<=12; $i++) {
-                                            <option value="{{$i}}">{{$i}}</option>
-                                        }
-                                        @endfor
-                                    </select>
-
-                                    月</h1>
+                            <div class="card-header center">
+                                作成するカレンダーの西暦と月を指定してください。
                             </div>
+
+                            <h1 class="center padding-top">
+
+                                <select name="selectedYear">
+                                    @for($i=0; $i<50; $i++) {
+                                        <option value="{{$thisYear + $i}}">{{$thisYear + $i}}</option>
+                                    }
+                                    @endfor
+                                </select>
+
+                                年
+
+                                <select name="selectedMonth">
+                                    @for($i=1; $i<=12; $i++) {
+                                        @if($i==$thisMonth)
+                                            <option value="{{$i}}" selected>{{$i}}</option>
+                                        @else
+                                            <option value="{{$i}}">{{$i}}</option>
+                                        @endif
+                                    }
+                                    @endfor
+                                </select>
+
+                                月</h1>
                         </div>
                 </div>
                     <div class="col-md-8">
-                        <div class="CandS">
-                            <h2>
-                                <a>キャンセル</a><a>保存</a>
-                            </h2>
+                        <div class="right">
+
+
+                            <a href="{{route('calendar.index')}}" class ="btn btn-light">キャンセル</a>
+                            <a href="" class ="btn btn-light">作成</a>
                         </div>
                     </div>
             </div>
