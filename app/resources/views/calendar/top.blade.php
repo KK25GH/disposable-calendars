92b043f9424e28f82763296c706c57f8bd8c5496
KAKU25GH <kakumorikoki25@gmail.com>
2024-02-12 17:10:15 +0900

    トップページを修正する

--- app/resources/views/calendar/top.blade.php
+++ app/resources/views/calendar/top.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row justify-content-center">
-        <a href="{{ route('calendar.index') }}" class="btn btn-light border">カレンダーを使用する</a>
-        <img class="resizeimg" src="https://i.imgur.com/8ZoRt5F.png" alt="GitHub README">
+    <div class="d-flex flex-column align-items-center">
+        <img class="text-center" src="https://i.imgur.com/y4yep47.png" alt="重複したカレンダーの図形">
+        <a href="{{ route('calendar.index') }}" class="btn btn-light w-25 m-3 border border-dark" role="button">カレンダーを使用する</a>
+        <h1>✅西暦・月を<u class="fw-bold">重複</u>してカレンダーを生成できる！</h1>
+        <ul class="m-5">
+            <li class="h2">予定や計画ごとにカレンダーを使用できます</li>
+            <li class="h2">使い捨てメモ感覚でカレンダーを使用できます</li>
+        </ul>
     </div>
 @endsection
